5b57d2b98448d826095724035ef4f1f818e3082e
dracarys1312 <dracarys1312@gmail.com>
2016-07-11 11:27:11 +0700

    Revert "chỉnh sửa thông tin khóa học và thêm file demo angular"
    This reverts commit 1899fa646db384682045b1b2c98f53cb2f71230e.

--- public/Techkids Connect/js/DemoAngularService.js
+++
@@ -1,8 +0,0 @@
-angular.module('companyService', []) // ten duoc dat trong app.routes.js
-    .factory('Company', function ($http) {
-        var companyFactory = {};
-        //get all jobs
-        companyFactory.all = function () {
-            return $http.get('http://125.212.233.51:2407/api/connect/company/');
-        };
-      }
