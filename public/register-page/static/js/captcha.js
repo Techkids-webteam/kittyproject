4ccf49cf8cb4fd89d44797a7217bc7f45baa5414
Pt <dracarys1312@gmail.com>
2016-07-29 12:44:39 +0700

    insert captcha in register-form

---
+++ public/register-page/static/js/captcha.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  var captcha = document.getElementById('rcaptcha');
+  var button_complete = document.getElementById('compelete');
+  var modal = document.getElementById('myModal');
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+  $('#form-android').on('submit', function(e) {
+    // 	var v = grecaptcha.getResponse();
+    // 	console.log(v);
+    // if(v.length == 0)
+    // {
+    // 		alert("You can't leave Captcha Code empty")
+    // }
+    // if(v.length != 0)
+    // {
+    // Prevent form submission
+    e.preventDefault();
+    $('#thong_bao').empty();
+    $('#thong_bao').append("Oops! ReCaptcha!");
+    $('#thank').empty();
+    modal.style.display = "block";
+    captcha.style.display = "block";
+    button_complete.style.display = "block";
+
+
+
+    // // Get the form instance
+    //      var $form = $(e.target);
+
+    //      // // Get the BootstrapValidator instance
+    //      var bv = $form.data($('#test-form'));
+
+    //      // Use Ajax to submit form data
+    //      var url = 'https://script.google.com/macros/s/AKfycbyZM_IQkLdiN_h-VRNYJjOADFr77c4Ekw3GAc-xLIRguaQ2iw/exec';
+    //      var redirectUrl = 'success-page.html';
+    //      // show the loading
+    //      $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
+
+    //      var jqxhr = $.post(url, $form.serialize(), function(data) {
+    //          console.log("Success! Data: " + data.statusText);
+    // modal.style.display = "block";
+    //      });
+    //     }
+
+  });
+
+  $('#compelete').on('click', function(a) {
+
+    var v = grecaptcha.getResponse();
+    console.log(v);
+    if (v.length == 0) {
+      alert("You can't leave Captcha Code empty");
+    }
+    if (v.length != 0) {
+      console.log("yes");
+      //var $form = $(e.target);
+      //var bv = $form.data($('#test-form'));
+      var url = 'https://script.google.com/macros/s/AKfycbx-njMOkqkeFSwKH9mKAJpTz00eUtJZCudEHc6c8q-c_qaoFww/exec';
+
+
+      var jqxhr = $.post(url, $('#test-form').serialize(), function(data) {
+        grecaptcha.reset();
+        captcha.style.display = "none";
+        button_complete.style.display = "none";
+        $('#thong_bao').empty();
+        $('#thong_bao').append("Đăng kí thành công!");
+        $('#thank').append("Cảm ơn bạn đã quan tâm!");
+        setTimeout(function() {
+          window.location.href = "register-successful";
+        }, 2000);
+        console.log("Success! Data: " + data.statusText);
+      });
+    }
+
+  });
+
+});
