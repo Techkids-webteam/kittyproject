0a821090dcdf903bf79d911a1629adf6b2a1d261
Pt <dracarys1312@gmail.com>
2016-08-08 17:43:17 +0700

    add Access-Control-Allow-Origin Header in app.js

--- app.js
+++ app.js
@@ -57,6 +57,11 @@ router.use(function (req, res, next) {
     next();
 });
 app.use('/', router);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 //========================================
 //<------------------static---------------------------------------------------->
 
