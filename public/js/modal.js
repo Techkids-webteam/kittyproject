8456cd9175f05ddaaf3269f61b3dbdf9e6098207
Pt <dracarys1312@gmail.com>
2016-08-11 20:36:32 +0700

    edit register modal in every course

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     var modal = document.getElementById('myModal');
+    var close = document.getElementById("close-btn");
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
+    }
+    close.onclick = function() {
+      modal.style.display = "none";
     }
     $('#submit-button-modal').on('click', function(e) {
         modal.style.display = "block";
