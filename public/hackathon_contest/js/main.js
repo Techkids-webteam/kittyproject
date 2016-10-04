e810d6c91cc657e41e0ba817716270dc632f71da
Pt <dracarys1312@gmail.com>
2016-10-04 18:17:40 +0700

    pretty much done layout

--- public/hackathon_contest/js/main.js
+++ public/hackathon_contest/js/main.js
@@ -1,5 +1,28 @@
-$('.single-item').slick();
+// SlideShow
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+// End SlideShow Code
+
+// FlipClock Code
 var clock;
 
 $(document).ready(function() {
@@ -20,3 +43,4 @@ $(document).ready(function() {
         showSeconds: false
     });
 });
+// End FlipClock Code
